4a2a79eb813232045a1a4a0364d9d6475210babe
Ben Matern <ben.matern@pirche.com>
2026-06-30 09:41:21 +0200

    working on samples

--- bin/XmlValidation.py
+++ bin/XmlValidation.py
@@ -1,26 +1,13 @@
 import argparse
+import xmlschema
 
-from lxml import etree
-from sys import exc_info
+def validate_against_schema(schema_filename, xml_filename):
+    schema = xmlschema.XMLSchema11(schema_filename)
+    if schema.is_valid(xml_filename):
+        print("Valid XML!")
+    else:
+        schema.validate(xml_filename) # Throws detailed error
 
-def validate_against_schema(schema_text, xml_text):
-    try:
-        # Validate XML against Schema.
-        schema_tree = etree.XMLSchema(etree.XML(schema_text))
-        xml_parser = etree.XMLParser(schema=schema_tree)
-        try:
-            # Encode the text to avoid encoding warnings
-            xml_tree = etree.fromstring(xml_text.encode('utf-8'), xml_parser)
-            # If we get this far, the XML has validated successfully.
-            return ('Valid')
-        except etree.XMLSyntaxError as err:
-            return ('Invalid:\n' + str(err))
-
-    except Exception as e:
-        print('Unexpected problem during xml validation.')
-        print(str(e))
-        print(exc_info())
-        return (str(e) + '\n' + str(exc_info()))
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -29,21 +16,10 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def read_file(file_name):
-    with open(file_name, 'r') as input_file:
-        file_text = input_file.read()
-        return file_text
-
 def main():
     args = parse_args()
-
-    xml_text = read_file(file_name=args.xml)
-    schema_text = read_file(file_name=args.xsd)
-
-    validation_results = validate_against_schema(schema_text=schema_text
-                                                 , xml_text=xml_text)
-
-    print(f'Validation Results:\n{validation_results}')
+    validate_against_schema(schema_filename=args.xsd
+                            , xml_filename=args.xml)
 
 if __name__ == '__main__':
     main()
